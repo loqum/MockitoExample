d797c347a01b2e202a71dd2dc1e0a7171edbed11
Ruben <ruben693@gmail.com>
2018-08-27 17:41:45 +0200

    commit final

--- src/test/java/com/arquitecturajava/test/TestServicioB.java
+++ src/test/java/com/arquitecturajava/test/TestServicioB.java
@@ -3,16 +3,44 @@ package com.arquitecturajava.test;
 import org.junit.Assert;
 import org.junit.Test;
 
+import com.arquitecturajava.servicios.ServicioA;
 import com.arquitecturajava.servicios.ServicioB;
+import com.arquitecturajava.servicios.ServicioAImpl;
 import com.arquitecturajava.servicios.ServicioBImpl;
 
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class TestServicioB {
+
+	@Test
+	public void testmultiplicarSumar() {
+
+		ServicioA servicioA = new ServicioAImpl();
+
+		ServicioB servicioB = new ServicioBImpl();
+
+		servicioB.setServicioA(servicioA);
+		Assert.assertEquals(servicioB.multiplicarSumar(2, 3, 2), 10);
+	}
 
 	@Test
 	public void testMultiplicacion() {
 
 		ServicioB servicioB = new ServicioBImpl();
-		Assert.assertEquals(servicioB.multiplicar(2, 3), 6);		
+		Assert.assertEquals(servicioB.multiplicar(2, 3), 6);
+
+	}
+
+	@Test
+	public void testmultiplicarSumar1() {
+
+		ServicioA servicioA = mock(ServicioA.class);
+		when(servicioA.sumar(2, 3)).thenReturn(5);
+
+		ServicioB servicioB = new ServicioBImpl();
+		servicioB.setServicioA(servicioA);
+		Assert.assertEquals(servicioB.multiplicarSumar(2, 3, 2), 10);
 
 	}
 
